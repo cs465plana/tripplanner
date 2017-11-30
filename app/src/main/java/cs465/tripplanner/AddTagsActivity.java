482e7a583acc9f0cfe5a445e357e987f0db451f9
teejas <tejas97siripurapu@gmail.com>
2017-11-29 21:52:48 -0600

    Added tags

--- app/src/main/java/cs465/tripplanner/AddTagsActivity.java
+++ app/src/main/java/cs465/tripplanner/AddTagsActivity.java
@@ -6,17 +6,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class AddTagsActivity extends AppCompatActivity implements View.OnClickListener{
+public class AddTagsActivity extends AppCompatActivity implements View.OnClickListener {
     private Button finishButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_budget);
+        setContentView(R.layout.activity_add_tags);
+
+        finishButton = (Button) findViewById(R.id.finish_button);
+        finishButton.setOnClickListener(this);
+        setTitle("Add Location");
     }
 
     public void onClick(View v) {
         if (v.getId() == R.id.finish_button) {
             Intent i = new Intent(this, HomeActivity.class);
             startActivity(i);
+        }
     }
 }
