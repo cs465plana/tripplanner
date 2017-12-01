1983bc6fa7b405e7733e4b06033452bf84a521e9
teejas <tejas97siripurapu@gmail.com>
2017-11-30 21:24:16 -0600

    Add Suggestions added

---
+++ app/src/main/java/cs465/tripplanner/AddSuggestionActivity.java
@@ -0,0 +1,13 @@
+package cs465.tripplanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddSuggestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_suggestion);
+    }
+}
