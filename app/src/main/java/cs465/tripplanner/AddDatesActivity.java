d19c9d86c2a973574d84bc9960d998ab92007980
Micky <miqueaskimhan@gmail.com>
2017-12-06 08:24:23 -0600

    date popup

--- app/src/main/java/cs465/tripplanner/AddDatesActivity.java
+++ app/src/main/java/cs465/tripplanner/AddDatesActivity.java
@@ -1,8 +1,31 @@
 package cs465.tripplanner;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class AddDatesActivity extends AddActivity {
+    EditText addDatesFrom= (EditText) findViewById(R.id.add_dates_from);
+    Calendar myCalendar = Calendar.getInstance();
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            // TODO Auto-generated method stub
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +36,26 @@ public class AddDatesActivity extends AddActivity {
         backClass = AddBudgetActivity.class;
         nextClass = AddTagsActivity.class;
         super.onCreate(savedInstanceState);
+
+        addDatesFrom.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(AddDatesActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
     }
 
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        addDatesFrom.setText(sdf.format(myCalendar.getTime()));
+    }
     @Override
     public void updateData() {
 
